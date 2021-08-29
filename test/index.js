bb91e818dc61f74e34cacdf7f6ec9c90202fdbf4
jobsyclass <77043592+jobsyclass@users.noreply.github.com>
2021-08-29 21:02:37 +0530

    Add files via upload
    For animation test

---
+++ test/index.js
@@ -0,0 +1,7 @@
+lottie.loadAnimation({
+    container: document.getElementById('demo'), // the dom element that will contain the animation: ;
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'test.json' // the path to the animation json
+  });
